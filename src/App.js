33346de6ddb115f594857ff8dafe83d0805c23f1
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2021-12-21 16:11:57 +0100

    module added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Module from "./components/Module";
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Modules</h1>
+      <Module />
+      <Module />
+      <Module />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Module.js
@@ -0,0 +1,11 @@
+function Module() {
+  return (
+    <div className="card">
+      <h2>Module 1</h2>
+      <div className="actions">
+        <button className="btn">Add to Schedule</button>
+      </div>
+    </div>
+  );
+}
+export default Module;
